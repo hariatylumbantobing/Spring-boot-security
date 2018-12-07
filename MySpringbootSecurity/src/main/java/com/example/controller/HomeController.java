d573dd80ec20339e46f5c3c98550cbd6c310c2ad
Hariaty Lumbantobing <44197421+hariatylumbantobing@users.noreply.github.com>
2018-12-07 21:22:10 +0700

    Add files via upload

---
+++ MySpringbootSecurity/src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String root() {
+	return "index";
+	}
+	
+	@GetMapping("/user")
+	public String userIndex() {
+	return "user/index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+	return "login";
+	}
+	
+	@GetMapping("/access-denied")
+	public String accessDenied() {
+	return "/error/access-denied";
+	}
+	
+	@GetMapping("/home")
+	public String home() {
+	return "home";
+	}
+	
+}
---
+++ MySpringbootSecurity/src/main/java/com/example/demo/MySpringbootSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootSecurityApplication.class, args);
+	}
+}
